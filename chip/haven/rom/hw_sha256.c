bf27fbc9a35124a667470de87a44b715e041654d
appleflyer <164886846+appleflyerv3@users.noreply.github.com>
2026-02-10 16:22:10 +0800

    fix: do not commit unfinished code!

--- chip/haven/rom/hw_sha256.c
+++ chip/haven/rom/hw_sha256.c
@@ -54,54 +54,22 @@ static void _sha_wait(uint32_t *digest)
 	while (!GREG32(KEYMGR, SHA_ITOP))
 		;
 
-	/* Read out final digest. */
-	digest[0] = GREG32(KEYMGR, SHA_STS_H0);
-	digest[1] = GREG32(KEYMGR, SHA_STS_H1);
-	digest[2] = GREG32(KEYMGR, SHA_STS_H2);
-	digest[3] = GREG32(KEYMGR, SHA_STS_H3);
-	digest[4] = GREG32(KEYMGR, SHA_STS_H4);
-	digest[5] = GREG32(KEYMGR, SHA_STS_H5);
-	digest[6] = GREG32(KEYMGR, SHA_STS_H6);
-	digest[7] = GREG32(KEYMGR, SHA_STS_H7);
+	for (i = 0; i < 8; ++i)
+		*digest++ = GREG32_ADDR(KEYMGR, SHA_STS_H0)[i];
+
+	GREG32(KEYMGR, SHA_ITOP) = 0;  /* Clear status. */
 }
 
 void hwSHA256(const void *data, size_t n, uint32_t *digest)
 {
 	GREG32(KEYMGR, SHA_ITOP) = 0;  /* Clear status. */
 
-	// something something
-
 	GREG32(KEYMGR, SHA_CFG_MSGLEN_LO) =  n;
 	GREG32(KEYMGR, SHA_CFG_MSGLEN_HI) = 0;
 
-	GREG32(KEYMGR, SHA_CFG_EN) = 1;
+	GREG32(KEYMGR, SHA_CFG_EN) = 0x10000;
 	GREG32(KEYMGR, SHA_TRIG) = 1;
 
 	_sha_write(data, n);
 	_sha_wait(digest);
-}
-
-void hw_sha256_init(void)
-{
-	GREG32(KEYMGR, SHA_ITOP) = 1;
-	// something something
-}
-
-void hw_sha256_update(void)
-{
-
-}
-
-void hw_sha256_update(const uint8_t *data, size_t n)
-{
-	if (!len)
-		return;
-
-	_sha_write(data, n);
-}
-
-void hw_sha256_final(uint32_t *out)
-{
-	GREG32(KEYMGR, SHA_TRIG) = 8;
-	_sha_wait(out);
 }
